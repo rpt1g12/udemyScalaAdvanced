24a78d525d2bfb7b43f891b6e6d2d0360249b6ab
Rafael Perez-Torro <rpereztorro@gmail.com>
2022-01-01 15:36:42 +0100

    [WIP] 2.12 Complement Set

--- src/main/scala/rpt/udemy/advancedScala/section3/FunctionalSets.scala
+++ src/main/scala/rpt/udemy/advancedScala/section3/FunctionalSets.scala
@@ -1,5 +1,48 @@
 package rpt.udemy.advancedScala.section3
 
-object FunctionalSets extends App{
+object FunctionalSets extends App {
 
+  val s = MySet(1,2,3,4)
+  // 1,2,3,4,5
+  println {
+    s + 5
+  }
+  // 1,2,3,4,5,6,7
+  println {
+    s ++ MySet(5,6,7)
+  }
+  // 10,100,20,200,30,300,40,400
+  println {
+    s map {
+      e => e*10
+    } flatMap {
+      e => MySet(e, e * 10)
+    }
+  }
+  // 1,3
+  println {
+    s"Filter: ${s filter (_%2==1)}"
+  }
+
+  // 4,3,1
+  println {
+    s"Substract: ${s - 2}"
+  }
+
+  // 1,4
+  println {
+    s"Difference: ${s -- MySet(3,2)}"
+  }
+
+  // 4, 3,2
+  println {
+    s"Intersection: ${s & MySet(3,2,4,5)}"
+  }
+
+  // 1,5
+  println {
+    val A = s
+    val B = MySet(3,2,4,5)
+    s"(A++B)--(A&B): ${(A++B)--(A&B)}"
+  }
 }
--- src/main/scala/rpt/udemy/advancedScala/section3/MySet.scala
+++ src/main/scala/rpt/udemy/advancedScala/section3/MySet.scala
@@ -1,6 +1,8 @@
 package rpt.udemy.advancedScala.section3
 
+import java.util.logging.{Level, Logger}
 import scala.annotation.tailrec
+import scala.util.Random
 
 /**
  * Custom Implementation of a functional set
@@ -8,6 +10,8 @@ import scala.annotation.tailrec
  * @tparam A Type of the elements contained in the set
  */
 trait MySet[A] extends (A => Boolean) {
+
+  protected val LOG: Logger = Logger.getLogger(this.getClass.getCanonicalName)
 
   override def apply(e: A): Boolean = contains(e)
 
@@ -22,6 +26,11 @@ trait MySet[A] extends (A => Boolean) {
    * @return True if the element is contained; false otherwise.
    */
   def contains(e: A): Boolean
+
+  /**
+   * Returns the complement of a set.
+   */
+  def unary_! : MySet[A]
 
   /**
    * Adds an element in to the set if its not already present.
@@ -79,6 +88,34 @@ trait MySet[A] extends (A => Boolean) {
    * @param func Function to apply to each element of the set.
    */
   def forEach(func: A => Unit): Unit
+
+  /**
+   * Removes a given element from the set.
+   *
+   * @param e Element to remove
+   * @return Set without the given element.
+   */
+  def - (e: A): MySet[A]
+
+  /**
+   * Returns the intersection between this set and another.
+   *
+   * @param other The other set to intersect this with.
+   * @return Intersection of the two sets where the resulting set only contains elements present in both sets.
+   */
+  def &(other: MySet[A]): MySet[A]
+
+  /**
+   * Returns the difference between this set and another.
+   *
+   * @param other Other set to remove from this.
+   * @return Returns the difference of the two sets where the resulting set contains elements only present in this set.
+   */
+  def --(other: MySet[A]): MySet[A]
+
+  override def toString(): String = strRepr
+
+  def strRepr: String
 }
 
 object MySet {
@@ -97,18 +134,77 @@ object MySet {
 }
 
 object EmptySetException extends RuntimeException("Trying to acces elements of an empty set.")
+object InfiniteSetException extends RuntimeException("Trying to acces elements of the infinite set.")
+
+class Complement[A](cons:MySet[A]) extends MySet[A] {
+
+  override def head: Nothing = throw InfiniteSetException
+
+  override def tail: MySet[A] = this
+
+  override def isEmpty: Boolean = false
+
+  override def contains(e: A): Boolean = !cons(e)
+
+  override def unary_! : MySet[A] = cons
+
+  override def +(e: A): MySet[A] = {
+    if cons contains e then
+      new Complement[A](cons - e)
+    else {
+      this
+    }
+  }
+
+  override def ++(other: MySet[A]): MySet[A] = new Complement[A](cons -- other)
+
+  override def map[B](mapFunction: A => B): MySet[B] = new Empty[B]
+
+  override def flatMap[B](flatMapFunction: A => MySet[B]): MySet[B] = new Empty[B]
+
+  override def filter(predicate: A => Boolean): MySet[A] = this
+
+  override def forEach(f: A => Unit): Unit = {}
+
+  override def -(e: A): MySet[A] = new Complement[A](cons+e)
+
+  override def &(other: MySet[A]): MySet[A] = other -- cons
+
+  override def --(other: MySet[A]): MySet[A] = new Complement[A](cons ++ other)
+
+  override def strRepr: String = s"!$cons"
+}
 
 class Empty[A] extends MySet[A] {
   override def head: Nothing = throw EmptySetException
+
   override def tail: MySet[A] = new Empty[A]
+
   override def isEmpty: Boolean = true
+
   override def contains(e: A): Boolean = false
+
+  override def unary_! : MySet[A] = new Complement[A](this)
+
   override def +(e: A): MySet[A] = Cons(e, this)
+
   override def ++(other: MySet[A]): MySet[A] = other
+
   override def map[B](mapFunction: A => B): MySet[B] = new Empty[B]
+
   override def flatMap[B](flatMapFunction: A => MySet[B]): MySet[B] = new Empty[B]
+
   override def filter(predicate: A => Boolean): MySet[A] = this
+
   override def forEach(f: A => Unit): Unit = {}
+
+  override def -(e: A): MySet[A] = this
+
+  override def &(other: MySet[A]): MySet[A] = this
+
+  override def --(other: MySet[A]): MySet[A] = this
+
+  override def strRepr: String = "{}"
 }
 
 case class Cons[A](head: A, tail: MySet[A]) extends MySet[A] {
@@ -119,6 +215,8 @@ case class Cons[A](head: A, tail: MySet[A]) extends MySet[A] {
       tail.contains(e)
     }
   }
+
+  override def unary_! : MySet[A] = new Complement[A](this)
 
   override def isEmpty: Boolean = false
 
@@ -182,7 +280,7 @@ case class Cons[A](head: A, tail: MySet[A]) extends MySet[A] {
         cum
       } else {
         if (predicate(rem.head)) {
-          cum + rem.head
+          helper(cum + rem.head, rem.tail)
         } else {
           helper(cum, rem.tail)
         }
@@ -192,7 +290,42 @@ case class Cons[A](head: A, tail: MySet[A]) extends MySet[A] {
     helper(cum = new Empty[A], this)
   }
 
-  override def forEach(func: A => Unit): Unit =
+  override def forEach(func: A => Unit): Unit = {
     func(head)
     tail.forEach(func)
+  }
+
+  override def strRepr: String = {
+    @tailrec
+    def helper(acc: Seq[String], rem: MySet[A]): Seq[String] = {
+      if rem.isEmpty then acc
+      else helper(acc = acc :+ rem.head.toString, rem.tail)
+    }
+
+    s"{${helper(Nil, this).mkString(", ")}}"
+  }
+
+  override def -(e: A): MySet[A] = {
+    @tailrec
+    def helper(rem: MySet[A], acc: MySet[A] = new Empty[A]) : MySet[A] = {
+      if rem contains e then {
+        if rem.head == e then {
+          tail ++ acc
+        } else {
+          helper(rem.tail,acc+rem.head)
+        }
+      } else {
+        rem
+      }
+    }
+    helper(this)
+  }
+
+  override def &(other: MySet[A]): MySet[A] ={
+    filter(!other)
+  }
+
+  override def --(other: MySet[A]): MySet[A] = {
+    filter(e => !(other contains e))
+  }
 }
