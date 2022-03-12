dd79c345faa4829eef1dbf83db3c1d7d7ba34d76
rperez-torro <rperez-torro@endeavorstreaming.com>
2022-03-12 22:41:40 +0000

    [ADD] JSONSerialization second attempt

--- src/main/scala/rpt/udemy/advancedScala/section5/JSONSerialization.scala
+++ src/main/scala/rpt/udemy/advancedScala/section5/JSONSerialization.scala
@@ -1,8 +1,8 @@
 package rpt.udemy.advancedScala.section5
 
 import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import java.util.Date
 
 object JSONSerialization extends App {
 
@@ -13,65 +13,51 @@ object JSONSerialization extends App {
   case class User(name: String, age: Int, email: String)
 
   object User {
-    val NAME="name"
-    val AGE="age"
-    val EMAIL="email"
-    implicit val jsonifier: Jsonifier[User] = (user:User) => {
-      JSONObject(
-        values = Map(
-          NAME -> JSONString(user.name),
-          AGE -> JSONNumber(user.age),
-          EMAIL -> JSONString(user.email)
-        )
-      )
+    import JSONConverter.implicits._
+    implicit object UserConverter extends JSONConverter[User] {
+      override def convert(value: User): JSONValue = {
+        val converter: JSONConverter[Product] = implicitly[JSONConverter[Product]]
+        converter.convert(value)
+      }
     }
-    implicit val jsonSerializer:JsonSerializer[User] = (user:User) => user.jsonify.stringify
   }
 
-  case class Post(content: String, createdAt: Date)
+  case class Post(content: String, createdAt: LocalDate)
 
   object Post {
-    val CONTENT="content"
-    val CREATED_AT="createdAt"
-    implicit val jsonifier: Jsonifier[Post] = (post:Post) => {
-      JSONObject(
-        Map(
-          CONTENT -> JSONString(post.content),
-          CREATED_AT -> JSONDate(post.createdAt)
-        )
-      )
+    import JSONConverter.implicits._
+    implicit object PostConverter extends JSONConverter[Post] {
+      override def convert(value: Post): JSONValue = {
+        val converter: JSONConverter[Product] = implicitly[JSONConverter[Product]]
+        converter.convert(value)
+      }
     }
-    implicit val jsonSerializer: JsonSerializer[Post] = (post:Post) => post.jsonify.stringify
   }
 
   case class Feed(user: User, posts: List[Post])
 
   object Feed {
-    val USER="user"
-    val POSTS="posts"
-    implicit val jsonifier: Jsonifier[Feed] = (feed:Feed) => {
-      JSONObject(
-        Map(
-          USER -> feed.user.jsonify,
-          POSTS -> JSONArray(feed.posts.map(_.jsonify))
-        )
-      )
+    import JSONConverter.implicits._
+    implicit object FeedConverter extends JSONConverter[Feed] {
+      override def convert(value: Feed): JSONValue = {
+        val converter: JSONConverter[Product] = implicitly[JSONConverter[Product]]
+        converter.convert(value)
+      }
     }
-    implicit val jsonSerializer: JsonSerializer[Feed] = (feed:Feed) => feed.jsonify.stringify
-
   }
 
   /*
-    We want to serialize them using TypeClass pattern.
+    We want to convert them using TypeClass pattern.
     Steps:
       1. Intermediate data types for: Int, Date, List, String
       2. TypeClass for conversion from case classes to intermediate types
-      3. serialize to JSON
+      3. convert to JSON
   */
 
   // 1. intermediate data type
   sealed trait JSONValue {
     def stringify: String
+    override def toString: String = stringify
   }
 
   final case class JSONString(value:String) extends JSONValue {
@@ -86,17 +72,15 @@ object JSONSerialization extends App {
     override def stringify: String = values.map(_.stringify).mkString("[",", ","]")
   }
 
-  final case class JSONDate(value:Date) extends JSONValue {
-    val formatter = new SimpleDateFormat("%y-%M-%d")
+  final case class JSONDate(value:LocalDate) extends JSONValue {
+    val formatter: DateTimeFormatter = DateTimeFormatter.ISO_DATE
     override def stringify: String = s"Date(${formatter.format(value)})"
   }
 
   final case class JSONObject(values: Map[String, JSONValue]) extends JSONValue {
-
     override def stringify: String = values.map {
         case (key, value) => s"\"$key\": ${value.stringify}"
       }.mkString("{",", ","}")
-
   }
 
   // test
@@ -111,38 +95,67 @@ object JSONSerialization extends App {
   println(jsonObject.stringify)
 
   // 2. Type Classes
-  trait Jsonifier[T] {
-    def jsonify(value: T):JSONValue
+  trait JSONConverter[T] {
+    def convert(value:T):JSONValue
   }
 
-  object Jsonifier {
-    def apply[T: Jsonifier](value:T):JSONValue = {
-      val jsonifier: Jsonifier[T] = implicitly[Jsonifier[T]]
-      jsonifier.jsonify(value)
+  object JSONConverter {
+    def apply[T: JSONConverter](value:T):JSONValue = {
+      val serializer: JSONConverter[T] = implicitly[JSONConverter[T]]
+      serializer.convert(value)
     }
-  }
-
-  trait JsonSerializer[T] {
-    def serialize(value:T):String
-  }
-
-  object JsonSerializer {
-    def apply[T: JsonSerializer](value:T):String = {
-      val serializer: JsonSerializer[T] = implicitly[JsonSerializer[T]]
-      serializer.serialize(value)
+    object implicits {
+      implicit object StringConverter extends JSONConverter[String] {
+        override def convert(value: String): JSONValue = JSONString(value)
+      }
+      implicit object IntConverter extends JSONConverter[Int] {
+        override def convert(value: Int): JSONValue = JSONNumber(value)
+      }
+      implicit object DateConverter extends JSONConverter[LocalDate] {
+        override def convert(value: LocalDate): JSONValue = JSONDate(value)
+      }
+      implicit object ListConverter extends JSONConverter[List[Any]] {
+        override def convert(value: List[Any]): JSONValue = {
+          JSONArray(
+            value.map {
+              case field:String => field.toJson
+              case field:Int => field.toJson
+              case field:LocalDate => field.toJson
+              case field:List[Any] => field.toJson
+              case field:Product => field.toJson
+            }
+          )
+        }
+      }
+      implicit object ProductConverter extends JSONConverter[Product] {
+        override def convert(value: Product): JSONValue = {
+          JSONObject(
+            Seq.range(0,value.productArity).map {
+              index => value.productElementName(index) -> value.productElement(index)
+            }.map {
+              case (key:String, field:String) => key -> field.toJson
+              case (key:String, field:Int) => key -> field.toJson
+              case (key:String, field:LocalDate) => key -> field.toJson
+              case (key:String, field:List[Any]) => key -> field.toJson
+              case (key:String, field:Product) => key -> field.toJson
+            }.toMap
+          )
+        }
+      }
     }
   }
 
   implicit class JsonEnrichment[T](value:T) {
-    def toJson(implicit serializer:JsonSerializer[T]):String = JsonSerializer[T](value)
-    def jsonify(implicit jsonifier: Jsonifier[T]):JSONValue = Jsonifier[T](value)
+    def toJson(implicit serializer:JSONConverter[T]):JSONValue = JSONConverter[T](value)
   }
 
   // Test
   val rafa = User("rafa", 32, "rpt@email.com")
-  val post1 = Post("Hello World!", new Date(2022,1,1))
-  val post2 = Post("Good Night World!", new Date(2022,12,31))
+  val post1 = Post("Hello World!", LocalDate.of(2022,1,1))
+  val post2 = Post("Good Night World!", LocalDate.of(2022,12,31))
   val feed = Feed(user = rafa, posts = List(post1, post2))
+
+
 
   println {
     feed.toJson
